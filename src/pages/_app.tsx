51288c6498783794bc66b726c29e389e980b9895
kiuyadeki <adeki.kiuy.19@gmail.com>
2024-06-30 21:41:42 +0900

    cognito認証画面を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import amplifyconfig from '../amplifyconfiguration.json';
+import { WithAuthenticatorProps, withAuthenticator } from '@aws-amplify/ui-react';
 
-const Myapp = ( { Component, pageProps}: AppProps) => {
+Amplify.configure(amplifyconfig);
+
+const Myapp = ( { Component, pageProps, signOut, user}: AppProps & WithAuthenticatorProps) => {
   return (
       <Component {...pageProps} />
   )
 }
 
-export default Myapp;
+export default withAuthenticator(Myapp);
